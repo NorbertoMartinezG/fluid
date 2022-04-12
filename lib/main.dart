188567ccb24c50ff8a5b348e06922633e5c15aa5
Drack <nautilus_com@yahoo.com>
2022-04-12 16:08:33 -0500

    Version final

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,19 @@
-//import 'dart:ffi';
 import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
-import 'package:vector_math/vector_math.dart' hide Colors; // Colors esta definida tanto en libreria material como en Vector
-
+import 'package:vector_math/vector_math.dart' hide Colors; // Colors esta definida  libreria material y en libreria Vector
 import 'package:sensors_plus/sensors_plus.dart';
-
-import 'package:flutter/services.dart'; // para evitar rotacion entre otros
-
-// import 'package:cpu_reader/cpu_reader.dart';
-// import 'package:cpu_reader/cpuinfo.dart';
-
+import 'package:flutter/services.dart'; // para evitar rotacion, entre otras funciones.
 
 // SPH fluid
 main() {
   
-  WidgetsFlutterBinding.ensureInitialized();// para evitar que gire la pantalla agregar libreria services
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])// para evitar que gire la pantalla agregar libreria services
-    .then((_) {// para evitar que gire la pantalla agregar libreria services
+  // para evitar giro en pantalla agregar libreria services
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+    .then((_) {
       runApp(new MaterialApp(home: new DemoPage()));
       });
 
@@ -51,10 +45,12 @@ class DemoBody extends StatefulWidget {
 
 class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
   AnimationController animationController;
-  final coleccionParticulas = <Particle>[];
-  final numParticulas = 1000;
-
-  double gyroX = 0;
+  
+  final coleccionParticulas = <Particle>[]; // inicializacion vector para particulas
+  final numParticulas = 10*10; // numero de particulas en implementacion
+  
+  // inicializacion variables para valores de sensores de posicion
+  double gyroX = 0; 
   double gyroY = 0;
   double gyroZ = 0;
 
@@ -63,7 +59,7 @@ class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
     super.initState();
 
     
-      // Para interaccion con girospio, acelerometro, // ESTO SOLO FUNCIONA CUANDO SE SELECCIONA EL EMULADOR pixel 3 XL API 29
+      // Para interaccion con girospio, acelerometro, // Solo funciona en android 10 o inferiores
       gyroscopeEvents.listen((GyroscopeEvent event) { // agregar libreria sensor_plus.dart
       setState(() {
 
@@ -77,32 +73,32 @@ class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
     
     double densidadReposo = 1000.0; // densidad en reposo
     double constanteGas = 2000.0; // const for equation of state
-    double H = 8.0; // kernel radius
-    double h2 = H * H; // radius^2 for optimization
-    double masa = 65.0; // assume all particles have the same masa
-    double viscosidad = 250.0; // viscosidadosity constant
-    double dt = 0.00080; // integration timestep
+    double H = 4.0; // kernel radius
+    double h2 = H * H; // radius^2 
+    double masa = 65.0; // masa para cada particula
+    double viscosidad = 250.0; // constante de viscosidad
+    double dt = 0.00080; // timestep
 
-// smoothing kernels defined in Müller and their gradients
+
+// nucleos de suavizado segun Müller
     double nucleoPoly6 = 315.0 / (65.0 * 3.1416 * pow(H, 9.0)); // El núcleo nucleoPoly6 también se conoce como núcleo polinomial de sexto grado. 
     double gradienteSpiky = -45.0 / (3.1416 * pow(H, 6.0)); // El gradiente del núcleo spiky se usa para calcular la fuerza de presión
     double laplacianoViscosity = 45.0 / (3.1416 * pow(H, 6.0)); // El Laplaciano de este núcleo (viscosidadosity) se usa para calcular la fuerza de viscosidad 
 
-// simulation parameters
-    double limitePantalla = H; // boundary limitePantallailon
-    double amortiguacion = -0.5; // amortiguacion 
-    Offset G = Offset(0.0, 12000 * 9.8);
+// parametros para simulacion
+    double limitePantalla = H; // limite Pantalla simulacion
+    double amortiguacion = -0.5; 
+    Offset G = Offset(0.0, 12000 * 9.8); // fuerza gravedad variable tipo offset para compatibilidad con variables nativas 
 
-    
-    
-    
+
+// Ordenacion inicial de particulas en formacion cuadrada
+    int num = sqrt(numParticulas).ceil();
     var listaxy = [];
     
-    for (var y = limitePantalla ; y < widget.screenSize.height - limitePantalla * 2.0; y += H) { // OPTIMIZAR -- YA QUE LEE TODA LA PANTALLA
-      Random random = new Random(); //nextDouble () genera un valor de punto flotante aleatorio distribuido entre 0.0 y 1.0. Aquí hay una pequeña función para simular un lanzamiento
-      for (var x = widget.screenSize.width / 4.0; x <= widget.screenSize.width / 2; x += H + 0.001*random.nextDouble()){
-         
-          listaxy.add([x,y]);
+    for (var y = limitePantalla ; y < widget.screenSize.height - limitePantalla * 2.0; y += H) { // orden en eje y
+      Random random = new Random(); //genera un valor de punto flotante aleatorio distribuido entre 0.0 y 1.0. Evita posiciones simetricas
+      for (var x = widget.screenSize.width / 4.0; x <= (widget.screenSize.width/4) + (H*num); x += H + 0.001*random.nextDouble()){ // orden en eje x aumentando valor random         
+          listaxy.add([x,y]); // vector de donde se tomaran los valores de posiciones iniciales
           
         }
         
@@ -114,7 +110,7 @@ class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
       double valx = (listaxy[i][0]); // variable para posicion inicial en x
       double valy = (listaxy[i][1]); // variable para posicion inicial en y
 
-      coleccionParticulas.add( // inicializacion de valores no inicializados en la calse Particle
+      coleccionParticulas.add( // inicializacion de valores no inicializados en la clase Particle
         new Particle(
           id: i, 
           screenSize: widget.screenSize, 
@@ -127,20 +123,11 @@ class _DemoBodyState extends State<DemoBody> with TickerProviderStateMixin {
       } 
     );
 
-// FutureBuilder<CpuInfo>(
-//                   future: CpuReader.cpuInfo,
-//                   builder: (context, AsyncSnapshot<CpuInfo> snapshot) =>
-//                       snapshot.hasData
-//                           ? Text(
-//                               'Number of cores: ${snapshot.data.numberOfCores}')
-//                           : Text('No data!'));
-
-
-
+// Funcion para calculo de nuevas posiciones de particulas
 void move(){
 
 
-          // Compute Density Pressure --------------------------------------------------
+        // Calculo densidad y presion para cada particula --------------------------------------------------
         for (int x = 0; x < coleccionParticulas.length; x++) 
         {
 
@@ -158,8 +145,9 @@ void move(){
          
         }
         
-        // ComputeForces -----------------------------------------------------------
+        // Calculo fuerzas -----------------------------------------------------------
          for (int x = 0; x < coleccionParticulas.length; x++) {
+           // Reinicializacion de fuerzas para otra particula
            Vector2 fpress = new Vector2(0.0,0.0);
            Offset fviscosidad = Offset(0, 0);
            Offset fpress2 = Offset(0.0, 0.0);
@@ -175,30 +163,28 @@ void move(){
              double r = rij2.normalize(); // norm en eigen c++
              if (r < H) {
 
-                // compute pressure force contribution de cada particula vecina
+                // ContributionContribucion de cada particula vecina 
                 // normalizar un vector es tomar un vector de cualquier longitud y, mientras sigue apuntando en la misma dirección, cambiar su longitud a 1, convirtiéndolo en lo que se conoce como un vector unitario.
                 // normalized() Normaliza un vector conocido en tiempo de compilación  Devuelve lo anterior como una copia construida, no afecta a la clase. Puede usarlo para asignar - Vector normCopy = vect.normalized().
-
                 fpress += -(rij2.normalized()) * masa * (coleccionParticulas[x].presion + coleccionParticulas[j].presion) / (2.0 * coleccionParticulas[j].densidad) * gradienteSpiky * pow(H - r, 2.0); 
-                
                 fpress2 = Offset(fpress[0],fpress[1]); // tomar valores de (fpress vector2) para (fpress2 offset)
-                
                 fviscosidad += ((coleccionParticulas[j].velocidad - coleccionParticulas[x].velocidad)/coleccionParticulas[j].densidad)* viscosidad * masa * laplacianoViscosity * (H - r); // variable offset 
                 
              }
            }
-            // ESTO SOLO FUNCIONA CUANDO SE SELECCIONA EL EMULADOR pixel 3  XL API 29
-            G = Offset(0.0, 12000 * 9.8 * (1 + (gyroX*3 + gyroY + gyroZ))); // gravedad 9.8 modificada temporalmente 
+            // Funcional solo en android 10 o inferiores.
+            G = Offset(0.0, 12000 * 9.8 * (1 + (gyroX*3 + gyroY + gyroZ))); // gravedad 9.8 modificada temporalmente por movimiento de dispositivo
             
             Offset fgrav = G * coleccionParticulas[x].densidad; // original
             coleccionParticulas[x].fuerzas = fgrav + fviscosidad + fpress2;
            
-            // COLOR DE PARTICULA SEGUN LA FUERZA EN CADA UNA DE ELLAS
+            // Color de particula segun la fuerza de PRESION  a la que se somete cada particula
             double fuerzaTotal = fpress2.dx + fpress2.dy ; // solo fuerza debida a presion.
-            double colorDinamico2 = fuerzaTotal/1000;
+            // escalar valores para compatibilidad con ARGB
+            double colorDinamico2 = fuerzaTotal/1000; 
             int colorDinamico3 = colorDinamico2.toInt(); 
             int colorDinamico4 = colorDinamico3.abs();
-            coleccionParticulas[x].notePaint.color=Color.fromARGB(255, 0, colorDinamico4, 255); // color dinamico segun fuerza
+            coleccionParticulas[x].notePaint.color=Color.fromARGB(255, 0, colorDinamico4, 255); // color dinamico segun fuerza de presion
          }
         
         // Integracion -------------------------------------------------------------
@@ -208,17 +194,14 @@ void move(){
 
               if (coleccionParticulas[x].position.dx - limitePantalla < 0.0) // si la posicion de la particula en el eje x menos H o es menor a 0(inicio del espacio de simulacion)  entonces:
               {
-                
                 double modVelx = 1.0;
                 modVelx *= (coleccionParticulas[x].velocidad.dx)*amortiguacion;
                 coleccionParticulas[x].velocidad = Offset(modVelx, coleccionParticulas[x].velocidad.dy);
-                
                 double modPosx = limitePantalla;
                 coleccionParticulas[x].position = Offset(modPosx, coleccionParticulas[x].position.dy);
                 
               }
               //posicionamiento en X global de la simulacion o ventana de simulacion
-		        
               if (coleccionParticulas[x].position.dx + limitePantalla > widget.screenSize.width ) // // si la posicion en el eje x de la particula mas H o es mayor al ancho de la simulacion entonces:
               {
                // desaceleracion por -0.5 que cambia la direccion de la velocidad
@@ -232,9 +215,8 @@ void move(){
               
               
               //posicionamiento en Y global de la simulacion o ventana de simulacion
-
               if (coleccionParticulas[x].position.dy - limitePantalla < 0.0) // si la posicion de la particula en el eje y menos H o es menor a 0(inicio del espacio de simulacion)  entonces:
-              {
+            {
                 //invierte la velocidad por -0.5f
                 double modVely = 1.0;
                 modVely *= (coleccionParticulas[x].velocidad.dy) * amortiguacion;
@@ -260,16 +242,9 @@ void move(){
               }
         }
 
-       
-
-
-
-
-
-
 }
 
-
+// configuracion animacion
     animationController = new AnimationController(
         vsync: this, duration: new Duration(seconds: 10))
       ..addListener(() {
@@ -280,7 +255,6 @@ void move(){
     )
       ..repeat();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -315,7 +289,7 @@ class _DemoPainter extends CustomPainter {
   bool shouldRepaint(_DemoPainter oldDelegate) => true;
 }
 
-
+// Clase particula con sus propiedades
 
 class Particle {
   int id;
@@ -334,10 +308,10 @@ class Particle {
   Offset velocidad;
 
   //Map<int, Particle> connected;
-
+  // Valores de clase particula a indicar antes de iniciar simulacion
   Particle(
       {@required this.id,
-      this.size = 6.0,//Tamaño del punto 
+      this.size = 3.0,//Tamaño del punto 
       this.radius = 100.0,
       @required this.position,
       @required this.densidad,
